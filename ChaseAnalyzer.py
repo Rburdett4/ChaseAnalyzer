31f963a5558c87865a6de8d4f333a7e821f2b99c
Ryan Burdett <rburdett@asu.edu>
2019-05-04 16:24:40 -0700

    Basic csv parsing

--- ChaseAnalyzer.py
+++ ChaseAnalyzer.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 # Author = Ryan Burdett
 # Contact = rburdett4@gmail.com
-print("Test2")
+import csv
+
+
+csvstring = "./Chase7917_Activity_20190504.CSV"        #in the future, this should be a GUI selection to make sure you can get any desired file.
+with open(csvstring, "r") as readfile:
+    reader = csv.reader(readfile)
+    for x in reader:
+        print(x)
+
